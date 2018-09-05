52c49aa43d96909f189d2074f761a1ccdec3d037
yinli <univeryinli@qq.com>
2018-09-05 09:57:13 -0400

    fix pre

--- pre_data.py
+++ pre_data.py
@@ -1,9 +1,11 @@
 import json
 import nltk
 import nltk.data
-from gensim import corpora
 from nltk.tokenize import RegexpTokenizer
 from compiler.ast import flatten
+import sys,os
+from gensim import corpora, models, similarities
+
 
 def splitSentence(paragraph):
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -17,10 +19,11 @@ def wordtokenizer(sentence):
 #    words=toker.tokenize(sentence)
     return words
 
-contents=[]
-titles=[]
-with open('./train_sample.txt') as fl:
-    for line in fl.readlines():
+def word2vector(path,stoplist):
+    file=open(path)
+    contents=[]
+    titles=[]
+    for line in file.readlines():
         line =json.loads(line)
         content=line['content'].replace('\n','.')
         title=line['title']
@@ -30,30 +33,38 @@ with open('./train_sample.txt') as fl:
         sentences_list=[]
         for sentence in sentences:
             words=wordtokenizer(sentence)
-            sentences_list.append(words)
+            words=[word.lower() for word in words if word not in stoplist]
+            sentences_list=sentences_list+words
         contents.append(sentences_list)
+    file.close()
+    return contents,titles
 
-stoplist=set('\ ,'.split())
-#sentences=flatten(contents)
-sentences=[]
-for content in contents:
-    sentences=sentences+content
-dictionary = corpora.Dictionary(sentences)
-print(dictionary.token2id)
-dictionary.save('./bytecup.dict')  # store the dictionary, for future reference
-corpus = [[dictionary.doc2bow(sentence) for sentence in content]for content in contents]
-for corp in corpus:
-    corpora.MmCorpus.serialize('./bytecup.mm', corp)  # store to disk, for later use
-    corpus = corpora.MmCorpus('./bytecup.mm')
-    print(corpus)
-corpus = corpora.MmCorpus('./bytecup.mm')
-#print(len(corpus[0]))
-file=open('./train_sample','w')
+path_base='../data/'
+path=path_base+'bytecup.corpus.train.txt'
+stoplist=set('\ , .  ( ) - ? / ! : ; !.'.split())
+contents,titles=word2vector(path,stoplist)
+dictionary = corpora.Dictionary(contents)
+#print(dictionary.token2id)
+dictionary.save(path_base+'bytecup.dict')  # store the dictionary, for future reference
+corpus_con = [dictionary.doc2bow(content) for content in contents]
+corpus_title=[dictionary.doc2bow(title) for title in titles]
+corpora.MmCorpus.serialize(path_base+'bytecup_content.mm', corpus_con)  # store to disk, for later use
+corpora.MmCorpus.serialize(path_base+'bytecup_title.mm', corpus_title)
+corpus_con = corpora.MmCorpus(path_base+'bytecup_content.mm')
+corpus_title = corpora.MmCorpus(path_base+'bytecup_title.mm')
+
+tfidf = models.TfidfModel(corpus_con)
+corpus_tfidf_con = tfidf[corpus_con]
+corpus_tfidf_title = tfidf[corpus_title]
+corpora.MmCorpus.serialize(path_base+'bytecup_tfidf_con.mm', corpus_tfidf_con)
+corpora.MmCorpus.serialize(path_base+'bytecup_tfidf_title.mm', corpus_tfidf_title)
+
+file=open(path_base+'train.data','w')
 file.write(str(contents))
 file.close()
-file=open('./lable_sample','w')
+file=open(path_base+'label.data','w')
 file.write(str(titles))
 file.close()
-file=open('./corpus','w')
-file.write(str(corpus))
-file.close()
+#file=open('./corpus','w')
+#file.write(str(corpus))
+#file.close()
