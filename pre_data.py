0ed8ee1119578794a6229fc3571c9f02dce40a47
yinli <univeryinli@qq.com>
2018-09-10 11:07:43 -0400

    add pre

--- pre_data.py
+++ pre_data.py
@@ -4,6 +4,7 @@ import nltk.data
 from nltk.tokenize import RegexpTokenizer
 #from compiler.ast import flatten
 import sys,os
+import numpy as np
 from gensim import corpora, models, similarities
 
 
@@ -25,7 +26,10 @@ def word2vector(path,stoplist):
     titles=[]
     for line in file.readlines():
         line =json.loads(line)
-        content=line['content'].replace('\n','.')
+        id=line['id']
+        print(id)
+        content=line['content'].replace('\"','"')
+        content=content.replace(u'\u2014','-').replace(u'\u201c','"').replace(u'\u201d','"').replace(u'\u2013','-').replace(u'\u2018',"'").replace(u'\u2019',"'")
         title=line['title']
         title=wordtokenizer(title)
         titles.append(title)
@@ -36,13 +40,40 @@ def word2vector(path,stoplist):
             words=[word.lower() for word in words if word not in stoplist]
             sentences_list=sentences_list+words
         contents.append(sentences_list)
+        title_content=sentences_list+title
     file.close()
-    return contents,titles
+    return contents,titles,title_content
 
 path_base='../data/'
-path=path_base+'bytecup.corpus.train.txt'
+path=path_base+'corpus.txt'
 stoplist=set('\ , .  ( ) - ? / ! : ; !.'.split())
-contents,titles=word2vector(path,stoplist)
+contents,titles,title_content=word2vector(path,stoplist)
+from gensim.models import Word2Vec
+model1 = Word2Vec(title_content, size=128, window=5, min_count=1, workers=100)
+model2 =Word2Vec(contents, size=128, window=5, min_count=1, workers=100)
+model1.save(path_base+'bytecup.1.models')
+model2.save(path_base+'bytecup.2.models')
+
+#suma=sum(model.wv.vectors)
+#t=['21','stories','our','readers','love','in','2017']
+#val=[]
+#for ts in t:
+#    val.append(model.wv[ts])
+#sumb=sum(np.array(val))
+#print(suma,sumb)
+
+def cos_len(a,b):
+    lena=np.sqrt(a.dot(a))
+    lenb=np.sqrt(b.dot(b))
+    coslen=a.dot(b)/(lena*lenb)
+    angel=np.arccos(coslen)
+    angel=angel*360/2/np.pi
+    return angel
+#print(model.similarity('love','loved'))
+#for title in titles[0]:
+#    print(model.wv[title.lower()])
+#   print(model.similarity())
+
 dictionary = corpora.Dictionary(contents)
 #print(dictionary.token2id)
 dictionary.save(path_base+'bytecup.dict')  # store the dictionary, for future reference
@@ -65,6 +96,6 @@ file.close()
 file=open(path_base+'label.data','w')
 file.write(str(titles))
 file.close()
-#file=open('./corpus','w')
-#file.write(str(corpus))
-#file.close()
+file=open(path_base+'title_content.data','w')
+file.write(str(title_content))
+file.close()
